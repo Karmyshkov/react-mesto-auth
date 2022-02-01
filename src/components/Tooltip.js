cb261f202906dcf1ab23b26b35f9a234ad35ce3b
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-02-01 22:09:58 +0700

    feat: add style for tooltip

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -6,7 +6,7 @@ const Tooltip = ({ status, isOpen, onClose }) => {
   console.log(status);
   return (
     <div className={`popup popup_type_more ${isOpen && "popup_opened"}`}>
-      <div className="popup__inner">
+      <div className="tooltip">
         <button
           onClick={() => onClose(false)}
           className="popup__close"
@@ -14,10 +14,15 @@ const Tooltip = ({ status, isOpen, onClose }) => {
           aria-label="Закрыть"
         ></button>
         <img
-          className="popup__img"
+          className="tooltip__img"
           src={status === 201 ? successIcon : errorIcon}
           alt={`${status === 201 ? "Успех" : "Ошибка"}`}
         />
+        <h2 className="tooltip__title">
+          {status === 201
+            ? "Вы успешно зарегистрировались!"
+            : "Что-то пошло не так! Попробуйте ещё раз."}
+        </h2>
       </div>
     </div>
   );
