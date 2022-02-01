21d52a61607f708ba63257fa009a24bd6a0ceadd
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-02-01 23:03:45 +0700

    feat: add useLocation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Footer = () => {
+  const location = useLocation();
   return (
     <footer className="footer">
-      <p className="footer__copyright">2021 Mesto Russia</p>
+      {location.pathname === "/" && (
+        <p className="footer__copyright">2021 Mesto Russia</p>
+      )}
     </footer>
   );
 };
