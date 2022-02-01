31255750fb803844b1714f96227dc375e729bcce
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-02-01 19:48:33 +0700

    feat: add handleLogin function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [dataForm, setDataForm] = useState({ password: "", email: "" });
+
+  const handleLogin = (evt) => {
+    setDataForm({ ...dataForm, [evt.target.name]: evt.target.value });
+  };
   return (
     <>
       <h2 className="login__title">Вход</h2>
-      <form className="login">
-        <input className="login__input" placeholder="Email" />
-        <input className="login__input" placeholder="Пароль" />
+      <form className="login" onSubmit={(evt) => evt.preventDefault()}>
+        <input
+          onChange={handleLogin}
+          className="login__input"
+          placeholder="Email"
+          name="email"
+        />
+        <input
+          onChange={handleLogin}
+          className="login__input"
+          placeholder="Пароль"
+          name="password"
+        />
         <button className="login__btn" type="submit">
           Войти
         </button>
