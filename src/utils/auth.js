5d98fbb80a116990e3359d56d1b18e816dea726c
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-02-01 20:35:06 +0700

    feat: add logic for login and register function

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,12 @@
 import { BASE_URL } from "./constants";
 
+const checkStatus = (res) => {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Error: ${res.status}`);
+};
+
 export const register = ({ password, email }) =>
   fetch(`${BASE_URL}/signup`, {
     method: "POST",
@@ -7,9 +14,10 @@ export const register = ({ password, email }) =>
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ password, email }),
-  }).then((res) => {
-    console.log(res);
-  });
+  })
+    .then((data) => checkStatus(data))
+    .then((dataUser) => console.log(dataUser))
+    .catch((err) => console.log(err));
 
 export const login = ({ password, email }) =>
   fetch(`${BASE_URL}/signin`, {
@@ -18,6 +26,7 @@ export const login = ({ password, email }) =>
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ password, email }),
-  }).then((res) => {
-    console.log(res);
-  });
+  })
+    .then((data) => checkStatus(data))
+    .then((dataUser) => console.log(dataUser))
+    .catch((err) => console.log(err));
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://auth.nomoreparties.co/signup";
+export const BASE_URL = "https://auth.nomoreparties.co";
